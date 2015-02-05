b47ef541b21bfabe44e9ca7a19bd90d913b09839
Xabier Larrakoetxea <slok69@gmail.com>
2015-02-05 21:03:44 +0100

    Bump version 1.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name = 'quantile',
-    version = '0.0.1',
+    version = '1.0',
     author = 'Matt T. Proud',
     author_email = 'matt.proud@gmail.com',
     description = ("Python Implementation of Graham Cormode and S. "
